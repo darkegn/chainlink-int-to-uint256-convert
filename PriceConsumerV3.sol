92d628721173f2dd1fa0c0ce075c434a7652255c
Darkegn <erdemegin34@gmail.com>
2023-01-19 23:13:58 +0300

    modified GetLatestPrice
    modified GetLatestPrice

---
+++ PriceConsumerV3.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+    }
+
+    /**
+     * Returns the latest price.  By Modified int -> Uint256
+     */
+    function getLatestPrice() public view returns (uint256) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint256(price)* (10**(18-8));
+    }
+}
